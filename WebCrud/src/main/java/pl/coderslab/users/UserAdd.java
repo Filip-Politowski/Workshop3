9426cd1425f6dd8d35820acc912a01eb788115c4
Filip-Politowski <fil.politowski@gmail.com>
2023-03-14 22:23:40 +0100

    Dodany główny szkielet w JSP

---
+++ WebCrud/src/main/java/pl/coderslab/users/UserAdd.java
@@ -0,0 +1,24 @@
+package pl.coderslab.users;
+
+import pl.coderslab.dao.UserDao;
+import pl.coderslab.entity.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "UserAdd", value = "/UserAdd")
+public class UserAdd extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        getServletContext().getRequestDispatcher("/users/add.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
